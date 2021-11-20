aa5df833460ca0e7c68b819e9a83269922d6fdc4
Sergio Marchena <sergiomarchena1999@gmail.com>
2021-11-20 21:35:24 +0100

    Frases actualizadas

--- Assets/_Scripts/InstructionsGenerator.cs
+++ Assets/_Scripts/InstructionsGenerator.cs
@@ -14,25 +14,25 @@ public class InstructionsGenerator : MonoBehaviour
     private string actualTask;
 
     [SerializeField] private TextMeshProUGUI taskText;
-
+    
     void Awake()
     {
         tasks = new List<string>();
 
         tasks.Add("Enciende el ordenador.");
         tasks.Add("Abre la ventana y bebe un refresco.");
-        tasks.Add("Coge la llamada del móvil, cambia la canción de la jukebox y los altavoces.");
-        tasks.Add("Enciende las luces, cierra la pueta y saca una foto.");
+        tasks.Add("Coge la llamada del móvil, enciende la jukebox y usa el portátil.");
+        tasks.Add("Enciende las luces, cierra la puerta y saca una foto.");
         tasks.Add("Apaga el ordenador, coge el móvil y no cierres la ventana.");
         tasks.Add("Apaga las luces, coge un refresco, déjalo en la barra y abre la puerta.");
         tasks.Add("Haz una foto, deja las luces apagadas, cierra la ventana y no cojas el móvil.");
         tasks.Add("Imprime un documento, enciende las luces, apaga el ordenador y abre la puerta.");
-        tasks.Add("Coge otro refresco y déjalo junto al pc, enciende los altavoces, no dejes la canción que está sonando y saca otra foto.");
-        tasks.Add("Enciende el ordenador,  apaga el ordenador y vuelve a encenderlo mientras estás en silencio.");
-        tasks.Add("Abre la ventana, coge el móvil, imprime otro documento y enciende los altavoces.");
+        tasks.Add("Coge otro refresco y déjalo junto al pc, apaga el portátil, no dejes que la canción siga sonando y saca otra foto.");
+        tasks.Add("Enciende el ordenador,  apaga el ordenador y vuelve a encenderlo mientras no estás en silencio.");
+        tasks.Add("Abre la ventana, coge el móvil, imprime otro documento y enciende el portátil.");
         tasks.Add("Cierra la puerta, apaga las luces, coge un refresco y déjalo junto al móvil, imprime otro documento más y no no apagues el ordenador.");
-        tasks.Add("Cierra la ventana, cambia la canción dos veces, no imprimas otro documento, usa el ordenador y bébete un refresco.");
-        tasks.Add("Saca dos fotos, coge la llamada, saca otra foto más, apaga los altavoces, enciende las luces y haz lo contrario en la siguiente tarea.");
+        tasks.Add("Cierra la ventana, no no no dejes encendida la jukebox, no imprimas otro documento, usa el ordenador y bébete un refresco.");
+        tasks.Add("Saca dos fotos, coge la llamada, saca otra foto más, apaga el portátil, enciende las luces y haz lo contrario en la siguiente tarea.");
         tasks.Add("No no no imprimas nada, deja cerrada la ventana y estate en silencio.");
     }
 
