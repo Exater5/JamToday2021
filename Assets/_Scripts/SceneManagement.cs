10a1ee7fa6936adcf26fe40ac345ecac52c03400
sergiomarchena1999 <sergiomarchena1999@gmail.com>
2021-11-20 12:27:00 +0100

    Distribución Menu

---
+++ Assets/_Scripts/SceneManagement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{/*
+    public static SceneManagement instance;
+
+    private void Awake() 
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+ 
+        instance = this;
+        DontDestroyOnLoad( this.gameObject );
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+    }*/
+}
