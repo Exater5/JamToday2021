0951de8c7f508f61ba684525e1c9c8a68010de46
torcu <45042365+Exater5@users.noreply.github.com>
2021-11-20 13:33:40 +0100

    PlayerMovement

--- Assets/ExternalAssets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
+++ Assets/ExternalAssets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class SimpleSampleCharacterControl : MonoBehaviour
@@ -17,7 +18,6 @@ public class SimpleSampleCharacterControl : MonoBehaviour
 
     [SerializeField] private float m_moveSpeed = 2;
     [SerializeField] private float m_turnSpeed = 200;
-    [SerializeField] private float m_jumpForce = 4;
 
     [SerializeField] private Animator m_animator = null;
     [SerializeField] private Rigidbody m_rigidBody = null;
@@ -27,73 +27,57 @@ public class SimpleSampleCharacterControl : MonoBehaviour
     private float m_currentV = 0;
     private float m_currentH = 0;
 
+    private bool _doingAction;
+
     private readonly float m_interpolation = 10;
     private readonly float m_walkScale = 0.33f;
-    private readonly float m_backwardsWalkScale = 0.16f;
-    private readonly float m_backwardRunScale = 0.66f;
 
     private Vector3 m_currentDirection = Vector3.zero;
+    private Camera _mainCamera;
 
-    private List<Collider> m_collisions = new List<Collider>();
 
     private void Awake()
     {
+        _mainCamera = Camera.main;
         if (!m_animator) { gameObject.GetComponent<Animator>(); }
         if (!m_rigidBody) { gameObject.GetComponent<Animator>(); }
     }
 
+    private void Start()
+    {
+        StartCoroutine(CrWave());
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && !_doingAction)
+        {
+            StartCoroutine(CrPickUp());
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && !_doingAction)
+        {
+            StartCoroutine(CrWave());
+        }
+    }
+
     private void FixedUpdate()
     {
-        switch (m_controlMode)
-        {
-            case ControlMode.Direct:
-                DirectUpdate();
-                break;
-
-            case ControlMode.Tank:
-                TankUpdate();
-                break;
-
-            default:
-                Debug.LogError("Unsupported state");
-                break;
-        }
+        DirectUpdate();
     }
-
-    private void TankUpdate()
-    {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
-
-        bool walk = Input.GetKey(KeyCode.LeftShift);
-
-        if (v < 0)
-        {
-            if (walk) { v *= m_backwardsWalkScale; }
-            else { v *= m_backwardRunScale; }
-        }
-        else if (walk)
-        {
-            v *= m_walkScale;
-        }
-
-        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
-
-        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
-        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
-
-        m_animator.SetFloat("MoveSpeed", m_currentV);
-    }
-
     private void DirectUpdate()
     {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
+        float v = 0;
+        float h = 0;
 
-        Transform camera = Camera.main.transform;
+        if (!_doingAction)
+        {
+            v = Input.GetAxis("Vertical");
+            h = Input.GetAxis("Horizontal");
+        }
 
-        if (Input.GetKey(KeyCode.LeftShift))
+
+        Transform camera = _mainCamera.transform;
+        if (!Input.GetKey(KeyCode.LeftShift))
         {
             v *= m_walkScale;
             h *= m_walkScale;
@@ -117,5 +101,20 @@ public class SimpleSampleCharacterControl : MonoBehaviour
 
             m_animator.SetFloat("MoveSpeed", direction.magnitude);
         }
+    }
+
+    public IEnumerator CrPickUp()
+    {
+        _doingAction = true;
+        m_animator.SetTrigger("Pickup");
+        yield return new WaitForSeconds(1.5f);
+        _doingAction = false;
+    }
+    public IEnumerator CrWave()
+    {
+        _doingAction = true;
+        m_animator.SetTrigger("Wave");
+        yield return new WaitForSeconds(2f);
+        _doingAction = false;
     }
 }
