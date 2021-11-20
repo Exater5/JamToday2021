13b1f5ed02754564f59c0533baf12ab3a3255a74
torcu <45042365+Exater5@users.noreply.github.com>
2021-11-20 18:16:02 +0100

    Update Sandbox

--- Assets/ExternalAssets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
+++ Assets/ExternalAssets/Supercyan Character Pack Free Sample/Scripts/SimpleSampleCharacterControl.cs
@@ -69,41 +69,36 @@ public class SimpleSampleCharacterControl : MonoBehaviour
     }
     private void DirectUpdate()
     {
-        float v = 0;
-        float h = 0;
-
         if (!_doingAction)
         {
-            v = Input.GetAxis("Vertical");
-            h = Input.GetAxis("Horizontal");
-        }
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
+            Transform camera = _mainCamera.transform;
+            if (!Input.GetKey(KeyCode.LeftShift))
+            {
+                v *= m_walkScale;
+                h *= m_walkScale;
+            }
 
+            m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+            m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
 
-        Transform camera = _mainCamera.transform;
-        if (!Input.GetKey(KeyCode.LeftShift))
-        {
-            v *= m_walkScale;
-            h *= m_walkScale;
-        }
+            Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
 
-        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
-        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+            float directionLength = direction.magnitude;
+            direction.y = 0;
+            direction = direction.normalized * directionLength;
 
-        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+            if (direction != Vector3.zero)
+            {
+                m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
 
-        float directionLength = direction.magnitude;
-        direction.y = 0;
-        direction = direction.normalized * directionLength;
+                transform.rotation = Quaternion.LookRotation(m_currentDirection);
+                transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
 
-        if (direction != Vector3.zero)
-        {
-            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
-
-            transform.rotation = Quaternion.LookRotation(m_currentDirection);
-            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
-
-            m_animator.SetFloat("MoveSpeed", direction.magnitude);
-        }
+                m_animator.SetFloat("MoveSpeed", direction.magnitude);
+            }
+        }     
     }
 
     public IEnumerator CrPickUp()
