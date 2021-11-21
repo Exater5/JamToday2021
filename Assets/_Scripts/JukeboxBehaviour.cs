775dc4c9355f62ea92e33f5ad069de271dbbbf35
sergiomarchena1999 <55439101+sergiomarchena1999@users.noreply.github.com>
2021-11-21 14:50:33 +0100

    Jukebox

---
+++ Assets/_Scripts/JukeboxBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JukeboxBehaviour : MonoBehaviour
+{
+    [SerializeField] private Gradient _gradient;
+    [SerializeField] private MeshRenderer _meshRenderer;
+    private Material _newMat;
+
+    [SerializeField] private bool _powered;
+
+    private void Start()
+    {
+        _meshRenderer.material = _newMat;
+        StartCoroutine(ChangeColor());
+    }
+
+    private IEnumerator ChangeColor()
+    {
+        float _time = 0;
+
+        while (_powered)
+        {
+            _time += Time.deltaTime;
+
+            if (_time > 1f)
+                _time = 0;
+
+            _meshRenderer.material.color = _gradient.Evaluate(_time);
+            yield return null;
+        }
+    }
+}
